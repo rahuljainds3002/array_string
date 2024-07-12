574145c87bd2c986e11b1fa9962f05aee48aea86
rahul jain <rahul.jainds3002@gmail.com>
2024-07-12 20:27:23 +0530

    question_7

---
+++ src/main/scala/question_7.scala
@@ -0,0 +1,14 @@
+object question_7 {
+  def main(args: Array[String]): Unit =
+    {
+      var arr = Array("String1","String2","String3","String4")
+      var string_check = "String3"
+      for(i<-0 until arr.length){
+        if (arr(i)==string_check){print("string is available")}
+
+      }
+
+
+    }
+
+}
